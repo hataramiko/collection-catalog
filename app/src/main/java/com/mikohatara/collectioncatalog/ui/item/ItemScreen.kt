d99fecef9adee75232161e508e848758c40ffddb
Miko Hatara <hatara.miko@gmail.com>
2024-04-05 14:01:35 +0300

    Looking into ModalNavigationDrawer

--- app/src/main/java/com/mikohatara/collectioncatalog/ui/item/ItemScreen.kt
+++ app/src/main/java/com/mikohatara/collectioncatalog/ui/item/ItemScreen.kt
@@ -1,2 +1,74 @@
 package com.mikohatara.collectioncatalog.ui.item
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mikohatara.collectioncatalog.ui.theme.CollectionCatalogTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "Item",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Edit,
+                            contentDescription = "Edit"
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = "More actions")
+                    }
+                }
+            )
+        },
+        content = { innerPadding ->
+            LazyColumn(
+                contentPadding = innerPadding,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                /*TODO*/
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun ItemScreenPreview() {
+    CollectionCatalogTheme {
+        ItemScreen()
+    }
+}
