cf88372fe71b4381fbf8df46dc952c5c0011935c
Miko Hatara <hatara.miko@gmail.com>
2024-04-11 17:15:54 +0300

    Home screen mock-up

--- app/src/main/java/com/mikohatara/collectioncatalog/ui/home/HomeScreen.kt
+++ app/src/main/java/com/mikohatara/collectioncatalog/ui/home/HomeScreen.kt
@@ -1,12 +1,21 @@
 package com.mikohatara.collectioncatalog.ui.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,20 +25,28 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.mikohatara.collectioncatalog.ui.item.ItemScreen
+import com.mikohatara.collectioncatalog.R
+import com.mikohatara.collectioncatalog.data.SampleImage
+import com.mikohatara.collectioncatalog.data.SampleImageSource
 import com.mikohatara.collectioncatalog.ui.theme.CollectionCatalogTheme
 
 @Composable
 fun HomeScreen() {
-    HomeTopAppBar()
+    HomeTopAppBar(itemList = SampleImageSource().loadSampleImages())
+    //HomeBody()
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeTopAppBar() {
+fun HomeTopAppBar(
+    itemList: List<SampleImage>,
+    modifier: Modifier = Modifier
+) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
     Scaffold(
@@ -79,10 +96,46 @@ fun HomeTopAppBar() {
                 contentPadding = innerPadding,
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                /*TODO*/
+                items(itemList) { item ->
+                    ItemCard(
+                        item = item,
+                        modifier = Modifier.padding(6.dp)
+                    )
+                }
             }
         }
     )
+}
+
+@Composable
+fun HomeBody(
+    itemList: List<SampleImage>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier) {
+        items(itemList) { item ->
+            ItemCard(
+                item = item,
+                modifier = Modifier.padding(6.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ItemCard(
+    item: SampleImage,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier) {
+        Image(
+            painter = painterResource(item.imageResourceId),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight(),
+            contentScale = ContentScale.FillWidth
+        )
+    }
 }
 
 @Preview
@@ -91,4 +144,10 @@ fun HomeScreenPreview() {
     CollectionCatalogTheme {
         HomeScreen()
     }
+}
+
+@Preview
+@Composable
+fun CardPreview() {
+    ItemCard(SampleImage(R.drawable.j_sa0123_a))
 }
