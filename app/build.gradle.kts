293062a312ed2bf1b87fba64e51164410e2c6f4c
Miko Hatara <hatara.miko@gmail.com>
2025-06-18 19:31:55 +0200

    0.1.0 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mikohatara.collectioncatalog"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.1"
+        versionCode = 3
+        versionName = "0.1.0"
 
         resourceConfigurations += setOf("en", "fi", "ja")
 
