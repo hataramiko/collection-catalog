3503275778413772a837a5cd765b50b3ed5bc999
Miko Hatara <hatara.miko@gmail.com>
2025-06-29 20:05:17 +0200

    Modify splash screen

--- app/src/main/java/com/mikohatara/collectioncatalog/MainActivity.kt
+++ app/src/main/java/com/mikohatara/collectioncatalog/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mikohatara.collectioncatalog.data.CollectionRepository
 import com.mikohatara.collectioncatalog.ui.theme.CollectionCatalogTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +17,7 @@ class MainActivity : ComponentActivity() {
     lateinit var collectionRepository: CollectionRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
