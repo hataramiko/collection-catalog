832aa618e50b3a766afeb433f9169115fc319317
Miko Hatara <hatara.miko@gmail.com>
2025-08-05 19:42:00 +0200

    Fix bottom sheets overlapping with status bar

--- app/src/main/java/com/mikohatara/collectioncatalog/ui/components/BottomSheets.kt
+++ app/src/main/java/com/mikohatara/collectioncatalog/ui/components/BottomSheets.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.selectable
@@ -81,7 +84,8 @@ fun SortByBottomSheet(
 
     ModalBottomSheet(
         onDismissRequest = { onDismiss() },
-        sheetState = sheetState
+        sheetState = sheetState,
+        modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars)
     ) {
         BottomSheetHeader(stringResource(R.string.sort_by))
         Column(
@@ -147,7 +151,9 @@ fun FilterBottomSheet(
     ModalBottomSheet(
         onDismissRequest = { onDismiss() },
         sheetState = sheetState,
-        modifier = Modifier.onGloballyPositioned { sheetHeight = it.size.height }
+        modifier = Modifier
+            .windowInsetsPadding(WindowInsets.statusBars)
+            .onGloballyPositioned { sheetHeight = it.size.height }
     ) {
         BottomSheetHeader(stringResource(R.string.filter))
         Box(
@@ -291,7 +297,8 @@ fun SettingsBottomSheet(
 
     ModalBottomSheet(
         onDismissRequest = { onDismiss() },
-        sheetState = sheetState
+        sheetState = sheetState,
+        modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars)
     ) {
         BottomSheetHeader(label, infoText)
         LazyColumn {
@@ -348,7 +355,8 @@ fun SelectCollectionBottomSheet(
 
     ModalBottomSheet(
         onDismissRequest = { onDismiss() },
-        sheetState = sheetState
+        sheetState = sheetState,
+        modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars)
     ) {
         BottomSheetHeader(stringResource(R.string.select_collection))
         LazyColumn {
