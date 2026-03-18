56018b6a78a9c999af62d9955d1c9597c93285b2
Miko Hatara <hatara.miko@gmail.com>
2026-03-18 18:09:08 +0100

    1.1.0 (18)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.mikohatara.collectioncatalog"
         minSdk = 24
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.0.1"
+        versionCode = 18
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
