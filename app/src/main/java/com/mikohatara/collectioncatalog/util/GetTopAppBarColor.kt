e91e3b27fd798b55c5449caab36822175d368632
Miko Hatara <hatara.miko@gmail.com>
2026-08-03 15:00:51 +0200

    Fine-tune TopRow appearance and behavior. Update empty list descriptions for collections

--- app/src/main/java/com/mikohatara/collectioncatalog/util/GetTopAppBarColor.kt
+++ app/src/main/java/com/mikohatara/collectioncatalog/util/GetTopAppBarColor.kt
@@ -2,9 +2,11 @@ package com.mikohatara.collectioncatalog.util
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.lerp
+import androidx.compose.ui.unit.dp
 import com.mikohatara.collectioncatalog.ui.components.customTopAppBarColors
 
 @Composable
@@ -15,11 +17,14 @@ fun getTopAppBarColor(
     collapsedFraction: Float
 ): Color {
     val isDarkTheme = isSystemInDarkTheme()
-    val collapsedAlpha = if (isDarkTheme) 0.5f else 0.9f
+    // The colors are close estimates made to match the system nav bar. Should be improved.
+    val (collapsedColor, collapsedAlpha) = if (isDarkTheme) {
+        colorScheme.surfaceColorAtElevation(4.dp) to 0.5f
+    } else colorScheme.surfaceContainerLowest to 0.9f
 
     return when {
-        isSelectionMode -> customTopAppBarColors().containerColor
-        isTopRowHidden -> colorScheme.surfaceContainer.copy(alpha = collapsedAlpha)
+        isSelectionMode -> colorScheme.surfaceContainerLow
+        isTopRowHidden -> collapsedColor.copy(alpha = collapsedAlpha)
         contentOffset < -1f -> customTopAppBarColors().scrolledContainerColor
         else -> lerp(
             customTopAppBarColors().containerColor,
